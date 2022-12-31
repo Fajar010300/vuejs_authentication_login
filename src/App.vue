bb1d71d3617e7d58b3612b7b30df77ff715b8de7
Fajar Setyo Pambudi <fajarsetyopambudi@gmail.com>
2022-12-31 13:03:20 +0700

    Push

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@
     <nav class="navbar navbar-expand navbar-light fixed-top">
       <div class="container">
         <a href="#" class="navbar-brand">Home</a>
-        <div class="collapse"></div>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Login</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Login</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   </div>
@@ -15,4 +24,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+/* @import url(<linkrel="stylesheet"href="https://fonts.googleapis.com/css?family=Tangerine">); */
+
+* {
+  box-sizing: border-box;
+}
+</style>
