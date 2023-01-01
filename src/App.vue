46705975571f582e01d1a3e9a5388d39c77785be
Fajar Setyo Pambudi <fajarsetyopambudi@gmail.com>
2023-01-01 11:27:15 +0700

    push

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand navbar-light fixed-top">
-      <div class="container">
-        <a href="#" class="navbar-brand">Home</a>
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a href="#" class="nav-link">Login</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">Login</a>
-            </li>
-          </ul>
-        </div>
+    <h1>{{ NavComponent }}</h1>
+    <div class="auth-wrapper">
+      <div class="auth-inner">
+        <HomeComponent />
       </div>
-    </nav>
+    </div>
   </div>
 </template>
 
 <script>
+import NavComponent from "./components/NavComponent.vue";
+import HomeComponent from "./components/HomeComponent.vue";
 export default {
   name: "App",
+  component: {
+    NavComponent,
+    HomeComponent,
+  },
 };
 </script>
 
@@ -29,5 +26,89 @@ export default {
 
 * {
   box-sizing: border-box;
+}
+
+body {
+  background-color: #ec355a;
+  min-height: 100vh;
+  display: flex;
+  font-weight: 400;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbar-brand {
+  text-align: center;
+  color: black;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+label,
+span {
+  font-weight: 500;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+body,
+html,
+#app,
+#root,
+.auth-wraper {
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  text-align: center;
+}
+
+.navbar-light {
+  background-color: #ffffff;
+  box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+}
+.auth-wraper {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: left;
+}
+.auth-inner {
+  width: 500px;
+  margin: auto;
+  background: #ffffff;
+  box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+  padding: 100px 75px 200px 80px;
+  border-radius: 15px;
+  transition: all 0.3s;
+}
+
+.auth-wraper .form-control:focus {
+  border-color: black;
+  box-shadow: none;
+}
+.auth-wraper h3 {
+  text-align: center;
+  margin: 0;
+  line-height: 1;
+  padding-bottom: 20px;
+}
+
+.custom-control-label {
+  font-weight: 400;
+}
+.forgot-password a {
+  text-align: right;
+  font-size: 13px;
+  padding-top: 10px;
+  color: blue;
+  margin: 0;
+}
+
+.forgot-password a {
+  color: red;
 }
 </style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hallo</h1>
+</template>
+
+<script>
+export default {
+  name: "HomeComponent",
+};
+</script>
+
+<style></style>
